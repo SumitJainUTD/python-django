bf8a737821fdbffe90c92aca7064b786dbea99c7
SumitJain <Sumit.Jain.12june@gmail.com>
2020-08-30 22:55:52 -0500

    post method

--- RestFWK/api_basic/views.py
+++ RestFWK/api_basic/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from .models import Article
 from .serializers import ArticleSerializer
@@ -8,12 +9,24 @@ import json
 
 # Create your views here.
 
-
+@csrf_exempt
 def article_list(request):
     if request.method == 'GET':
+        print("get call ARTICLE")
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
         return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        print("post call Article")
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
 
 
 def home_page(request):
