e792c337d3a1d0afbb5c5348c564413e9a24b826
SumitJain <Sumit.Jain.12june@gmail.com>
2020-08-30 23:04:04 -0500

    post working

--- RestFWK/api_basic/urls.py
+++ RestFWK/api_basic/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import article_list, home_page
+from django.views.decorators.csrf import csrf_exempt
+
 
 urlpatterns = [
     path('article/', article_list),
