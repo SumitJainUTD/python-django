b6a2f2971b5f453f2ec36ddfecf613f02a38bee7
SumitJain <Sumit.Jain.12june@gmail.com>
2020-09-07 16:50:18 -0500

    more chnages

--- RestFWK/api_basic/urls.py
+++ RestFWK/api_basic/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import article_list, home_page
+from .views import article_list, home_page, article_detail, ArticleAPIView, ArticleDetailView
 from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
-    path('article/', article_list),
+    # path('article/', article_list),
     path('', home_page),
+    # path('detail/<int:pk>', article_detail),
+    path('article/', ArticleAPIView.as_view()),
+    path('detail/<int:id>', ArticleDetailView.as_view()),
 ]
